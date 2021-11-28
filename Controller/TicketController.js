7082972ad9dd5d5e967f827d1f32ef7406210692
Salma Kishk <salma.o.kishk@gmail.com>
2021-11-28 18:28:57 +0200

    fixed frontend overlapping in manage flights

--- Controller/TicketController.js
+++ Controller/TicketController.js
@@ -2,42 +2,34 @@ const router = require("express").Router();
 let Flight = require("../Models/Tickets");
 
 router.route("/CreateTicket").post((req, res) => {
-    const AwayFlight = req.body.AwayFlight;
-    const ReturnFlight = req.body.ReturnFlight;
-    const AwayCabin = req.body.AwayCabin;
-    const ReturnCabin = req.body.ReturnCabin;
-    const AwayPrice = req.body.AwayPrice;
-    const ReturnPrice = req.body.ReturnPrice;
-    const AgeGroup = req.body.AgeGroup;
-    
-  
-  
-    const newTicket = new Ticket({
-      AwayFlight,
-      ReturnFlight,
-      AwayCabin,
-      ReturnCabin,
-      AwayPrice,
-      ReturnPrice,
-      AgeGroup
-    });
-  
-    newTicket
-      .save()
-      .then(() => res.json("Ticket CReated!"))
-      .catch((err) => res.status(400).json("Error: " + err));
+  const AwayFlight = req.body.AwayFlight;
+  const ReturnFlight = req.body.ReturnFlight;
+  const AwayCabin = req.body.AwayCabin;
+  const ReturnCabin = req.body.ReturnCabin;
+  const AwayPrice = req.body.AwayPrice;
+  const ReturnPrice = req.body.ReturnPrice;
+  const AgeGroup = req.body.AgeGroup;
+
+  const newTicket = new Ticket({
+    AwayFlight,
+    ReturnFlight,
+    AwayCabin,
+    ReturnCabin,
+    AwayPrice,
+    ReturnPrice,
+    AgeGroup,
   });
-  router.route("/ChooseSeat").post((req, res) => {
-    Tickets.findByIdAndUpdate(
-      req.body._id,
-      req.body.SeatNumber,
-      function (err) {
-        if (err) console.log(err);
-        console.log("Ticket updated successfully");
-        console.log(req.body.id);
-        
-      }
-      
-    );
-    res.send();
+
+  newTicket
+    .save()
+    .then(() => res.json("Ticket Created!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+router.route("/ChooseSeat").post((req, res) => {
+  Tickets.findByIdAndUpdate(req.body._id, req.body.SeatNumber, function (err) {
+    if (err) console.log(err);
+    console.log("Ticket updated successfully");
+    console.log(req.body.id);
   });
+  res.send();
+});
