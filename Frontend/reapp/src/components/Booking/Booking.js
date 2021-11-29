65c23dce433fe5832712931a8c8da3ba565d32b6
faridahelmy <faridahelmy6@gmail.com>
2021-11-30 00:31:52 +0200

    Fixed Backend Ticket

--- Frontend/reapp/src/components/Booking/Booking.js
+++ Frontend/reapp/src/components/Booking/Booking.js
@@ -1,7 +1,29 @@
 import "./Booking.css";
 import BoardingPass from "./BoardingPass";
 
-function Booking() {
+function Booking(props) {
+ const flightHandler=()=>{
+  axios
+  .post("http://localhost:8000/Tickets/findMyTickets", props.UserID)
+  .then((res) => {
+   
+    res.map((ticket)=> {
+
+
+      return (
+        <BoardingPass
+        departureAirport = {}
+        />
+          
+
+      );
+    })
+  })
+  .catch((e) => {
+    alert("error");
+    console.log(e);
+  });
+}
   return (
     <div className="booking">
         <div className="booking-top">
