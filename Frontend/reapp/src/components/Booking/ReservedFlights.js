e2ce97d3a6b5bf020d012a2e0ff677f19cef19cf
Salma Kishk <salma.o.kishk@gmail.com>
2021-12-03 17:18:24 +0200

    added reserved flights no code

---
+++ Frontend/reapp/src/components/Booking/ReservedFlights.js
@@ -0,0 +1,7 @@
+import "./ReservedFlights.css";
+import BoardingPass from "./BoardingPass";
+import Booking from "./Booking";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function ReservedFlights(props) {}
