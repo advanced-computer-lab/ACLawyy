e79d097a002c8ab976054976ff9f60119a225314
Salma Kishk <salma.o.kishk@gmail.com>
2021-12-25 13:44:12 +0200

    fixed sme syntactical sugar via prettier

--- Frontend/reapp/src/App.js
+++ Frontend/reapp/src/App.js
@@ -7,14 +7,13 @@ import UserHome from "./components/UserHome/UserHome";
 import UserPage from "./components/UserPage/UserPage";
 import ReservedFlights from "./components/Booking/ReservedFlights";
 import Seats from "./components/Seats/Seats";
-import Payment from "./components/Payment/Payment"
-import Login from "./components/Login/Login"
-import Register from "./components/Register/Register"
-import BookAFlight from "./components/BookAFlight/BookAFlight"
+import Payment from "./components/Payment/Payment";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import BookAFlight from "./components/BookAFlight/BookAFlight";
 import BottomPage from "./components/UserHomePage/BottomPage/BottomPage";
 
-
-import {ReactSession} from 'react-client-session';
+import { ReactSession } from "react-client-session";
 import {
   BrowserRouter as Router,
   Route,
@@ -22,26 +21,21 @@ import {
   Link,
 } from "react-router-dom";
 
-
 ReactSession.setStoreType("localStorage");
 
 function App() {
   const [currPage, setCurrPage] = useState(0);
   const [seatProps, setSeatProps] = useState({});
- // const [userID,setUserID] = useState (null);
+  // const [userID,setUserID] = useState (null);
 
- //const [userType, setUserType] = useState(2);
-
-
+  //const [userType, setUserType] = useState(2);
 
   const setUserID = (id) => {
-
     ReactSession.set("id", id);
-  }
+  };
   const setUserType = (type) => {
-
     ReactSession.set("userType", type);
-  }
+  };
 
   // useEffect (()=> {
   //    ReactSession.set("userType", 2);
@@ -50,45 +44,55 @@ function App() {
     <Router>
       <div>
         <Switch>
-          <Route exact path="/"   element={
-          <UserHome UserID={ReactSession.get("id")}/>}></Route>
-          <Route exact path="/booking"   element={<BookAFlight UserID={ReactSession.get("id")}/>}></Route>
-          
+          <Route
+            exact
+            path="/"
+            element={<UserHome UserID={ReactSession.get("id")} />}
+          ></Route>
+          <Route
+            exact
+            path="/booking"
+            element={<BookAFlight UserID={ReactSession.get("id")} />}
+          ></Route>
+
           <Route
             exact
             path="/profile"
             element={<UserPage userID={ReactSession.get("id")} />}
           ></Route>
-          <Route
-            path="/payment/:purchaseBody"
-            element={<Payment/>}></Route>
+          <Route path="/payment/:purchaseBody" element={<Payment />}></Route>
           <Route
             exact
             path="/reservedflights"
-            element={<ReservedFlights userID={ReactSession.get("id")} onSeats={setSeatProps} />}
+            element={
+              <ReservedFlights
+                userID={ReactSession.get("id")}
+                onSeats={setSeatProps}
+              />
+            }
           ></Route>
-         <Route
+          <Route
             exact
             path="/login"
-            element={<Login  setUserID = {setUserID} setUserType = {setUserType}/>}
+            element={<Login setUserID={setUserID} setUserType={setUserType} />}
           ></Route>
-           <Route
-            exact
-            path="/register"
-            element={<Register/>}
-          ></Route>
+          <Route exact path="/register" element={<Register />}></Route>
           <Route path="/chooseSeats/:seatParams" element={<Seats />}></Route>
 
-          {ReactSession.get("userType")==0&&<Route path="/adminhome" element={ <AdminHome />}></Route>}
+          {ReactSession.get("userType") == 0 && (
+            <Route path="/adminhome" element={<AdminHome />}></Route>
+          )}
 
-          {ReactSession.get("userType")==0&&<Route path="/manageflights" element={ <AdminPage />}></Route>}
-          </Switch>
+          {ReactSession.get("userType") == 0 && (
+            <Route path="/manageflights" element={<AdminPage />}></Route>
+          )}
+        </Switch>
       </div>
-      <NavBar type={ReactSession.get("userType")}  />
+      <NavBar type={ReactSession.get("userType")} />
       <BottomPage class="BottomPage" />
     </Router>
   );
-  
+
   return (
     <div style={{ height: 400, width: "100%" }}>
       <NavBar type="0" goTo={setCurrPage} />
